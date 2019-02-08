742ce937d63b56ee913e208605d02e2c0a03ed30
Sun Lijun <ashther@qq.com>
2019-02-08 20:51:23 +0800

    reduce dependency

--- R/rotatelog.R
+++ R/rotatelog.R
@@ -14,31 +14,50 @@
 #' at most, and with only \code{backou_n} log files in this log directory
 #'
 #' @importFrom  luzlogr openlog closelog printlog
-#' @importFrom stringr str_extract str_replace
-#' @importFrom fs file_info path_dir dir_ls file_delete file_move
 #'
 #' @export
 rotatelog <- function(log_name, msg, max_bytes = 200*1000, backup_n = 5) {
 
-  if (file_info(log_name)$size >= max_bytes) {
+  if (file.size(log_name) >= max_bytes) {
 
-    log_files_path <- path_dir(log_name)
-    log_files <- dir_ls(log_files_path, regexp = paste0(basename(log_name), '\\.?\\d*'))
-
-    ind <- as.integer(str_extract(log_files, '\\d+$'))
+    log_files_path <- dirname(log_name)
+    log_files <- list.files(
+      log_files_path,
+      pattern = paste0(basename(log_name), '\\.?\\d*'),
+      full.names = TRUE
+    )
+    ind <- gregexpr('\\d+$', log_files)
+    ind <- vapply(seq_along(ind), FUN = function(i) {
+      match_length <- attr(ind[[i]], 'match.length')
+      match_start <- as.integer(ind[[i]])
+      if (match_length > 0)
+        as.integer(substr(log_files[i], match_start, match_start + match_length))
+      else
+        NA_integer_
+    }, FUN.VALUE = integer(1))
 
     # delete extra log files bigger than backup_n
-    file_delete(log_files[!is.na(ind) & ind >= backup_n])
+    file.remove(log_files[!is.na(ind) & ind >= backup_n])
 
     # rename log files, add 1 to everyone which already in log.1 type
     valid_ind <- !is.na(ind) & ind >= 1 & ind < backup_n
-    files_from <- log_files[valid_ind]
-    files_to <- str_replace(files_from, '\\d+$', as.character(ind[valid_ind] + 1))
-    file_move(rev(files_from), rev(files_to))
+    if (any(valid_ind)) {
+      files_from <- log_files[valid_ind]
+      files_to <- file.path(
+        log_files_path, paste0(basename(log_name), '.', ind[valid_ind] + 1)
+      )
+      ind_order <- order(ind[valid_ind], decreasing = TRUE)
+      files_from <- files_from[ind_order]
+      files_to <- files_to[ind_order]
+
+      for (i in seq_along(files_from)) {
+        file.rename(files_from[i], files_to[i])
+      }
+    }
 
     # rename log file to log.1, and re-create new log file
     closelog(FALSE)
-    file_move(log_name, paste0(log_name, '.1'))
+    file.rename(log_name, paste0(log_name, '.1'))
     openlog(log_name, append = TRUE, sink = TRUE)
     printlog(msg)
 
