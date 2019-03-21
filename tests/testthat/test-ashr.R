8b5a37a37a210731c4d24357e02f92dcd0b9788d
sunlijun <ashther@qq.com>
2019-03-21 19:02:12 +0800

    add unit test about dailylog

--- tests/testthat/test-ashr.R
+++ tests/testthat/test-ashr.R
@@ -1,12 +1,15 @@
 context("test-ashr")
 
 test_that("setting and getting", {
-  print('setting')
 
-  openlog('log/test')
-  expect_equal(getLogName(), 'log/test')
+  log_name <- file.path(tempdir(), 'log/log')
+  openlog(log_name)
+  expect_equal(getLogName(), log_name)
+
+  expect_false(suppressWarnings(file.remove(log_name)))
+
   closelog()
-  unlink('log', TRUE)
+  unlink(dirname(getLogName()), TRUE)
 
   setBackupN(6)
   expect_equal(getBackupN(), 6)
@@ -28,7 +31,6 @@ test_that("setting and getting", {
 })
 
 test_that('print to log file', {
-  print('printlog')
 
   log_name <- file.path(tempdir(), 'log/log')
 
@@ -44,7 +46,6 @@ test_that('print to log file', {
 })
 
 test_that('rotate log', {
-  print('rotatelog')
 
   log_name <- file.path(tempdir(), 'log/log')
 
@@ -75,6 +76,44 @@ test_that('rotate log', {
   temp <- list.files(dirname(getLogName()))
   expect_equal(length(temp), 4)
   expect_equal(temp, c('log', 'log.1', 'log.2', 'log.3'))
+
+  closelog()
+  unlink(dirname(getLogName()), TRUE)
+})
+
+test_that('daily log', {
+  msg_test <- 'write message to an old log file'
+
+  log_name <- file.path(tempdir(), 'log/log')
+  openlog(log_name)
+
+  dailylog(msg_test)
+  temp <- readLines(getLogName())
+  temp <- strsplit(temp, '\\s\\]\\s')[[1]][2]
+  temp <- trimws(temp)
+  expect_equal(temp, msg_test)
+
+  if (Sys.info()['sysname'] == 'Windows') {
+    res <- system(sprintf(
+      "powershell -command (Get-ChildItem %s).CreationTime = '06/22/1986 12:42AM'",
+      log_name
+    ), intern = TRUE)
+    expect_equal(res, character(0))
+
+    dailylog(msg_test)
+    temp <- list.files(dirname(getLogName()))
+    expect_equal(temp, c('log', 'log.1986-06-22'))
+    file.remove(file.path(dirname(log_name), 'log.1986-06-22'))
+  }
+
+  file.create(file.path(dirname(log_name), 'log.2000-01-01'))
+  file.create(file.path(dirname(log_name), 'log.2000-01-02'))
+  file.create(file.path(dirname(log_name), 'log.2000-01-03'))
+  file.create(file.path(dirname(log_name), 'log.2000-01-04'))
+  setBackupN(2)
+  dailylog(msg_test)
+  temp <- list.files(dirname(getLogName()))
+  expect_equal(temp, c('log', 'log.2000-01-03', 'log.2000-01-04'))
 
   closelog()
   unlink(dirname(getLogName()), TRUE)
